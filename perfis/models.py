8263cae09fcee774889f5f478eaa9be2ce3cc639
Rute Miliana <rutemiliana13@gmail.com>
2022-11-06 20:03:47 -0300

    Gerando paginas dinamica

--- perfis/models.py
+++ perfis/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Perfil(object):
+    #construtor
+    def __init__(self, nome='', email='', telefone='', nome_empresa=''):
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+        self.nome_empresa = nome_empresa
+
--- perfis/views.py
+++ perfis/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from perfis.models import Perfil
 
 # Create your views here.
 
@@ -7,5 +8,13 @@ def index(request):
     return render(request , 'index.html')
 
 def exibir(request, perfil_id):
-    print('ID do perfil recebido: {}'.format(perfil_id))
-    return render(request , 'perfil.html')
+
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Rute Miliana', 'rute@gmail.com', '666-666', 'pitanguinha')
+    if perfil_id == 2:
+        perfil = Perfil('Jailson Mendes', 'dlccara@gmail.com', '666-666', 'Delicia')
+
+    return render(request , 'perfil.html' , {'perfil' : perfil})
+    ## terceiro parâmetro para a função render, que nada mais é do que um dicionário que contém a chave "perfil", que guarda o objeto perfil 
