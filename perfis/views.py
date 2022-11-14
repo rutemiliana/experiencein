417767198f96702055c1379b9048a3ce8ec699ab
Rute Miliana <rutemiliana13@gmail.com>
2022-11-14 12:12:10 -0300

    removendo a instancia do Perfil e das duas clausulas if

--- perfis/views.py
+++ perfis/views.py
@@ -9,12 +9,5 @@ def index(request):
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Rute Miliana', 'rute@gmail.com', '666-666', 'pitanguinha')
-    if perfil_id == 2:
-        perfil = Perfil('Jailson Mendes', 'dlccara@gmail.com', '666-666', 'Delicia')
-
     return render(request , 'perfil.html' , {'perfil' : perfil})
     ## terceiro parâmetro para a função render, que nada mais é do que um dicionário que contém a chave "perfil", que guarda o objeto perfil 
