4d8fc1543ac458d31c55be1d7b069a867bb4c32d
Rute Miliana <rutemiliana13@gmail.com>
2022-11-14 11:24:33 -0300

    herdando de models

--- perfis/models.py
+++ perfis/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Perfil(object):
-    #construtor
+class Perfil(models.Model): #Nome da tabela (ORM)
+    #construtor, campos da tabela
     def __init__(self, nome='', email='', telefone='', nome_empresa=''):
         self.nome = nome
         self.email = email
